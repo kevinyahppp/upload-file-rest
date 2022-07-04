887e25d79aba6858bed69f560949047a229d56e8
kevin <kevinyahppp@gmail.com>
2022-07-04 02:20:01 -0500

    change endpoint controller

--- src/main/java/com/keywords/controllers/FileController.java
+++ src/main/java/com/keywords/controllers/FileController.java
@@ -3,10 +3,12 @@ package com.keywords.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/resume")
 public class FileController {
-    @PostMapping("upload-file")
+    @PostMapping("/upload-file")
     public ResponseEntity<String> uploadFile() {
         return ResponseEntity.ok("ok");
     }
